fb668a820fbba76823fba0ba766b115091d26106
Carlos Wilson Niqx <wkabiru19@gmail.com>
2023-07-12 19:42:40 +0300

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,6 @@
--- Lists all bands with Glam rock as their style,
--- ranked by their longevity
--- Column names must be: band_name & lifespan
-
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY lifespan DESC;
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
