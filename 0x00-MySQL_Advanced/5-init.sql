ae143938b88fac4fa47eef7d3053e306868d7b60
Carlos Wilson Niqx <wkabiru19@gmail.com>
2023-07-12 12:07:32 +0300

    Create 5-init.sql

---
+++ 0x00-MySQL_Advanced/5-init.sql
@@ -0,0 +1,12 @@
+-- Initial
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+    id int not null AUTO_INCREMENT,
+    email varchar(255) not null,
+    name varchar(255),
+    valid_email boolean not null default 0,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO users (email, name) VALUES ("wkabiru19@gmail.com", "Wilsonkabiru");
